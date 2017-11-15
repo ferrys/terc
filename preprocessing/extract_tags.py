5fc12e1816ad20aea234f56d28453958f758d2a5
ferrys <ferrys@bu.edu>
2017-11-14 20:13:41 -0500

    updated preprocessing code

--- preprocessing/extract_tags.py
+++ preprocessing/extract_tags.py
@@ -2,9 +2,10 @@ import subprocess
 import glob
 
 wanted_tags = ["Volcano", "Sunrise Sunset", "ISS Structure", "Stars", "Night", "Aurora", "Movie", "Day", "Moon", "Inside ISS", "Dock Undock", "Cupola"]
-filenames = glob.glob("./BUSampleDataSet/*.jpg")
+filenames = glob.glob("../../BUSampleDataSet/*.jpg")
 
 csv_str = ""
+max_tags = 0
 for filename in filenames:
     try: # use ExifTool from http://www.sno.phy.queensu.ca/~phil/exiftool/
         output = subprocess.check_output(['/usr/local/bin/exiftool', "-Subject", filename])
@@ -14,13 +15,25 @@ for filename in filenames:
     if all_tags:
         tags = ""
         all_tags = all_tags.split(", ")
+        count = 0
         for tag in all_tags:
             if tag in wanted_tags:
                 tags += tag + ","
+                count += 1
+        if count > max_tags:
+            max_tags = count
         print(tags)
+        #windows
+        # filename = filename[filename.rindex("\\")+1:]
+
+        #mac
         filename = filename[filename.rindex("/")+1:]
         csv_str += filename + "," + tags[:-1] + "\n"
-csv_str = "id\n" + csv_str
+print(max_tags)
+tag_columns = ""
+for i in range(max_tags):
+    tag_columns += "tag_" + str(i) + ","
+csv_str = "id,"+ tag_columns[:-1] + "\n" + csv_str
 with open('extracted_tags.csv', 'w') as f:
     f.write(csv_str)
     f.close()
